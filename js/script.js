eacaef15e95642a8d1f5868d5134793b64e73a0d
Alex Ferreira <alex.ferreira1796@gmail.com>
2021-03-31 17:44:05 -0300

    Slide paginacao

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-import { SlideNav } from './modules/slide.js';
+import SlideNav from './modules/slide.js';
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
-
 slide.addArrow('.prev', '.next');
-
+slide.addControl('.custom-controls');
